e55f4f56d4827377f6cebeb1fc4acc03c8ee3033
linh4 <linh44h@gmail.com>
2018-10-08 00:38:30 -0400

    create app

---
+++ src/components/Titles.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export default class Titles extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Weather</h1>
+        <p>temp</p>
+      </div>
+    );
+  }
+}
